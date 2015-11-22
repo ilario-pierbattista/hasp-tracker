80de54c8bc78e9ae6366f6adb0182b8f0ff0e59f
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2015-11-22 18:12:11 +0100

    max accuracy

--- matlab/old/hasp/strongClassifier.m
+++
@@ -1,9 +0,0 @@
-function precence = strongClassifier(weaks, weights, threshold)
-if ~isequal(size(weaks), size(weights))
-    error('myfuns:strongClassifier:badArgument',...
-    'Il vettore delle ipotesi e quello dei pesi devono essere ',...
-    'della stessa lunghezza')
-end
-
-value = weaks * transpose(weights);
-precence = value > threshold;
